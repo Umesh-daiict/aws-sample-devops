1f687ce87643be36115b33ac8d528b865bb95efe
umesh <umeshsavaliya007@gmail.com>
2025-04-09 11:42:04 +0530

    change the code

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from AWS CodeDeploy!\n');
+  res.end('Hey, it is working!\n ');
 });
 
 server.listen(port, hostname, () => {
